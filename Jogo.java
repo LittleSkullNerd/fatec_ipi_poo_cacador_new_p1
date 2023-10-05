c6c2ab5711316fd6fba00184b1bd578c3b0ea2b0
Milena Hayakawa <mirenahayakawa@gmail.com>
2023-10-04 21:52:30 -0300

    p1(jogo): batalha de personagens

--- Jogo.java
+++ Jogo.java
@@ -1,19 +1,22 @@
 import java.util.Random;
 
+// classe jogo que simula o comportamento de dois personagens
 public class Jogo {
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception { 
+    //objetos da classe Personagem
     var gerador = new Random();
-    var personagem1 = new Personagem("John");
-    var personagem2 = new Personagem("Karla");
+    var personagem1 = new Personagem("Gaunter");
+    var personagem2 = new Personagem("Zyra");
 
     //variavel que inicia como true para fazer a troca de personagens no jogo
     boolean alterarTurno = true;
 
+    //loop infinito até uma condição for quebrada(se ambos morrerem)
     while (true) {
-      int oQueFazer = gerador.nextInt(4);
-    
+      int oQueFazer = gerador.nextInt(4); //gera numero aleatorio entre 0 e 4, para as ações dos personagens
       if (alterarTurno){
-        // Ação do personagem "John"
+
+        // Ação do personagem "Gaunter"
         switch (oQueFazer) {
           case 0:
             personagem1.cacar();
@@ -35,7 +38,7 @@ public class Jogo {
 
       } else {
         
-        // Ação do personagem "Karla"
+        // Ação do personagem "Zyra"
         switch (oQueFazer) {
           case 0:
             personagem2.cacar();
@@ -62,11 +65,11 @@ public class Jogo {
         break;
       }
 
-      // Alternar entre os personagens na próxima iteração
+      // Alternar entre os personagens, invertendo os valores true para false, e vice-versa
       alterarTurno = !alterarTurno;
 
       System.out.println("--------------------------------");
-      Thread.sleep(1000);
+      Thread.sleep(1000); //pequena pausa entre os turnos
     }
     
   }
