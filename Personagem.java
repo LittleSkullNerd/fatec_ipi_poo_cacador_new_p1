31a8d733fec3f8175b5a0c85ef5a8c8a943cf6fe
Milena Hayakawa <mirenahayakawa@gmail.com>
2023-10-04 21:51:33 -0300

    p1(jogo): batalha de personagens

--- Personagem.java
+++ Personagem.java
@@ -1,16 +1,18 @@
 public class Personagem {
  
-  //atributos ou propriedades
-  private String nome;
-  private int energia = 10; //inicia com total de energia 
-  private int fome = 0; //sem fome
-  private int sono = 0;
-  private int item = 4;//sem sono
+  //atributos ou propriedades da classe Personagem
+  private String nome;      //nome do tipo string
+  private int energia = 10; //inicia com total de energia = 10
+  private int fome = 0;     //começa sem fome
+  private int sono = 0;     //começa sem sono
+  private int item = 4;     //item inicia com 4
 
+  //Construtor da classe personagem
   Personagem(String nome){
     this.nome = nome;
   }
 
+  //Construtor da classe personagem, responsavel por construir o objeto (criar uma instancia)
   Personagem(String nome, int energia, int fome, int sono){
     this.nome = nome;
     if (energia >= 0 && energia <= 10)
@@ -21,32 +23,35 @@ public class Personagem {
       this.sono = sono;  
   }
 
-  //comportamentos (métodos)
+  //comportamentos / métodos a seguir
+  //método caçar
   void cacar(){
     if(energia <= 0){
-      System.out.println(nome + " está morto e nao pode caçar");
+      System.out.println(nome + " está morto e não pode caçar");
       return;
     }
     if(energia >= 2){
       System.out.println(nome + " caçando...");
-      energia -= 2; // energia = energia - 2;
+      energia -= 2;     //energia = energia - 2;
       item++;
     } else {
       System.out.println (nome + " sem energia para caçar...");
     }
-    fome = Math.min(fome + 1, 10);
+    fome = Math.min(fome + 1, 10);  //função math.min para garantir que nao excedam o valor 10
     sono = Math.min(sono + 1 ,10);
   }
 
+  //método comer
   void comer(){
     if(energia <= 0){
-      System.out.println(nome + " está morto e nao pode comer");
+      System.out.println(nome + " está morto e não pode comer");
       return;
     }
     if (fome >= 1 && item >= 1){
       System.out.println(nome + " comendo...");
       // operador ternário
       energia = energia + 1 > 10 ? 10 : energia + 1;
+      //
       fome--;
       item--;
     }
@@ -58,9 +63,10 @@ public class Personagem {
     }
   }
 
+  //método dormir
   void dormir(){
     if(energia <= 0){
-      System.out.println(nome + " está morto e nao pode dormir");
+      System.out.println(nome + " está morto e não pode dormir");
       return;
     }
     if(sono > 0){
@@ -73,6 +79,7 @@ public class Personagem {
     }
   }
 
+  //método exibir o estado do personagem, retornando alguns valores
   void exibirEstado(){
     System.out.printf(
       "%s: e: %d, f: %d, s: %d, i: %d\n",
@@ -84,6 +91,7 @@ public class Personagem {
     );
   }
 
+  //método morrer
   void morrer(){
   if(energia <= 0){
     System.out.println(nome + " Game Over para " + nome);
@@ -93,6 +101,7 @@ public class Personagem {
     System.out.println(nome + " Continua vivo ");
   }
 
+  //método diminuir energia
   void diminuirEnergia(){
     this.energia--;
     if(this.energia <= 0){
@@ -100,10 +109,10 @@ public class Personagem {
     }
   }
 
-
+  //método atacar
   void atacar(Personagem alvo){
     if(energia <= 0){
-      System.out.println(nome + " está morto e nao pode atacar");
+      System.out.println(nome + " está morto e não pode atacar");
     }
     if(alvo.energia <= 0){
       System.out.println(alvo.nome + " já esta morto " + nome + " não pode atacar um personagem morto");
@@ -114,14 +123,14 @@ public class Personagem {
       alvo.diminuirEnergia();
     }
     else{
-      System.out.println(nome + " Sem energia para atacar ");
+      System.out.println(nome + " sem energia para atacar ");
     }
   }
 
+//método de acesso que retorna valor energia
 public int getEnergia() {
     return energia;
 }
-
 }
 
 
